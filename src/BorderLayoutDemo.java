bcd4f3896e7ccb1c97577f2dc86e6e5d786a50d4
guillhermek <119710522+guillhermek@users.noreply.github.com>
2023-05-11 15:49:20 -0300

    Update BorderLayoutDemo.java

--- src/BorderLayoutDemo.java
+++ src/BorderLayoutDemo.java
@@ -1,10 +1,13 @@
 import javax.swing.JFrame;
-public class BorderLayoutDemo
-{
-public static void main(String[] args) {
-    BorderLayoutFrame borderLayoutFrame = new BorderLayoutFrame();
-    borderLayoutFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-    borderLayoutFrame.setSize( 300, 200 );
-    borderLayoutFrame.setVisible( true );  
+
+ /* ALUNO IFMT Guilherme Vinicius da Silva 
+  * CODIGO: https://www.slideshare.net/secret/HrtIHOmPpKpmBc
+  */ 
+public class BorderLayoutDemo {
+    public static void main(String[] args) {
+            BorderLayoutFrame borderLayoutFrame = new BorderLayoutFrame();
+            borderLayoutFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+            borderLayoutFrame.setSize( 300, 200 );
+            borderLayoutFrame.setVisible( true );  
     } 
 } 
